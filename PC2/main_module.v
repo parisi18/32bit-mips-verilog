b03cff7239524936e1d8d153e54bf662a5cd760a
ParisiBR <parisi18@unifesp.br>
2024-07-03 22:06:35 -0300

    Initial files

---
+++ PC2/main_module.v
@@ -0,0 +1,31 @@
+/*Módulo principal do MIPS 32 bits*/
+/*
+INSTRUÇÕES SELECIONADAS:
+Lógicas:
+    - ADD
+    - SUB
+    - MULT
+    - DIV
+Aritméticas:
+    - AND
+    - OR
+    - Shift Left
+    - Shift Right
+Transferência de Dados:
+    - LOAD
+    - STORE
+Branchs condicionais:
+    - BEQ
+    - BNE
+Comparação:
+    - SLT
+Jump incondicional:
+    - JUMP
+    - JAL
+*/
+
+module Main();
+
+
+
+endmodule
---
+++ PC2/pc.v
@@ -0,0 +1,7 @@
+/*MIPS 32 Instruction Fetching*/
+
+module Pc();
+
+
+
+endmodule
