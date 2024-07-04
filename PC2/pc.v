414102ac19ec8111d468dc82fc43f2784473c81a
ParisiBR <parisi18@unifesp.br>
2024-07-03 22:19:03 -0300

    módulo PC

--- PC2/pc.v
+++ PC2/pc.v
@@ -2,10 +2,13 @@
 
 module Pc(addr, pc_out);
 
-input [31:0] addr;
-output [31:0] pc_out;
+input [31:0] addr_in;
+output reg [31:0] pc_out;
 
-
-
+reg addr_out;
+always @(*) begin 
+    assign addr_out = addr_in + 4;
+    assign pc_out = addr_out; 
+end
 
 endmodule
